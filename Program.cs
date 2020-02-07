4a0835de37ea46bb231b801354f00425bd66e0e0
Lawrence Gripper <info@grippers.co.uk>
2020-02-07 16:46:46 +0000

    Move to azdo cloner

--- Program.cs
+++ Program.cs
@@ -1,184 +1,139 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
 using System.Collections.Generic;
 using System.Linq;
+using LibGit2Sharp;
+using System.IO;
+using LibGit2Sharp.Handlers;
 
 namespace adoprloadbalancer
 {
-
-    class PullRequest
-    {
-        public GitPullRequest PR;
-        public List<IdentityRefWithVote> Reviewers;
-    }
-
-    class ReviewersHistory
-    {
-        public bool IsActive;
-        public string UserId;
-        public int PastReviewCount;
-        public int ActiveReviewsCount;
-        public int TotalReviews => (PastReviewCount + ActiveReviewsCount);
-    }
-
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            if (args.Length == 3)
+            Uri org1Url = new Uri("https://dev.azure.com/" + Environment.GetEnvironmentVariable("ORG1"));
+            String project1 = Environment.GetEnvironmentVariable("PROJ1");
+            String pat1 = Environment.GetEnvironmentVariable("PAT1");  // See https://docs.microsoft.com/azure/devops/integrate/get-started/authentication/pats
+
+            Uri org2Url = new Uri("https://dev.azure.com/" + Environment.GetEnvironmentVariable("ORG2"));
+            String project2 = Environment.GetEnvironmentVariable("PROJ2");
+            String pat2 = Environment.GetEnvironmentVariable("PAT2");
+
+            // Project name which this should look at
+            Boolean dryRun = !String.IsNullOrEmpty(Environment.GetEnvironmentVariable("DRYRUN"));
+
+            // Create a connection
+            var sourceConnection = new VssConnection(org1Url, new VssBasicCredential(string.Empty, pat1));
+            var destinationConnection = new VssConnection(org2Url, new VssBasicCredential(string.Empty, pat2));
+
+            var destProject = await destinationConnection.GetClient<ProjectHttpClient>().GetProject(project2);
+
+
+            var sourceRepos = await GetAllRepos(sourceConnection, project1);
+            var destReposDict = (await GetAllRepos(destinationConnection, project2)).ToDictionary(x => x.Name);
+
+            var missingReposInDest = sourceRepos.Where(x => !destReposDict.ContainsKey(getDestNameFromSourceName(x.Name)));
+
+            var tempPath = Path.GetTempPath();
+            var tempDir = Directory.CreateDirectory(Path.Join(tempPath, "repocloner", RandomString(5).ToLower()));
+
+            Console.WriteLine($"Using path {tempDir.FullName}");
+
+            // Create any repos missing in proj2
+            var destSourceClient = destinationConnection.GetClient<GitHttpClient>();
+            foreach (var sourceRepo in missingReposInDest)
             {
-                Uri orgUrl = new Uri(args[0]);         // Organization URL, for example: https://dev.azure.com/fabrikam               
-                String personalAccessToken = args[1];  // See https://docs.microsoft.com/azure/devops/integrate/get-started/authentication/pats
-                String project = args[2];              // Project name which this should look at
-
-                // Create a connection
-                VssConnection connection = new VssConnection(orgUrl, new VssBasicCredential(string.Empty, personalAccessToken));
-
-                AssignMissingReviewers(connection, project, targetReviewerCount: 2).Wait();
-            }
-            else
-            {
-                Console.WriteLine("Usage: ConsoleApp {orgUrl} {personalAccessToken} {projectName}");
-            }
-
-        }
-
-        static private async Task AssignMissingReviewers(VssConnection connection, String project, int targetReviewerCount)
-        {
-            var prs = await GetAllActivePRs(connection, project);
-
-            Dictionary<string, ReviewersHistory> reviewersHistory = BuildHistory(prs);
-
-            var prsNeedingReviewers = prs.Where(pr => pr.Reviewers.Count() < 2 && pr.PR.Status == PullRequestStatus.Active);
-
-            foreach (var pr in prsNeedingReviewers)
-            {
-                while (pr.Reviewers.Count() < 2)
+                if (dryRun)
                 {
-                    var reviewerToAssign = GetNextReviewer(reviewersHistory.Values);
-                    
-                    if (reviewerToAssign == null) {
-                        Console.WriteLine("We ran out of reviewers to assign. Will wait for next invocation and try agian");
-                        Environment.Exit(1);
-                    }
+                    Console.WriteLine($"Would create repo {sourceRepo.Name} in {destSourceClient.BaseAddress}");
+                }
+                else
+                {
 
-                    Console.WriteLine($"Would assign user {reviewerToAssign.UserId} to review {pr.PR.Title}");
-
-                    // Todo: Actually set user on PR
-                    
-                    if (pr.Reviewers?.Count() > 0)
+                    var gitToCreate = new GitRepositoryCreateOptions()
                     {
-                        pr.Reviewers.Add(new IdentityRefWithVote(new IdentityRef { Id = reviewerToAssign.UserId }));
+                        Name = getDestNameFromSourceName(sourceRepo.Name),
+                        ProjectReference = destProject,
+                    };
+                    var destRepo = await destSourceClient.CreateRepositoryAsync(gitToCreate);
+                    destReposDict[destRepo.Name] = destRepo;
+                }
+            }
+
+            // Sync source from proj1 to proj2 for all branchs
+            foreach (var sourceRepo in sourceRepos)
+            {
+                var destRepo = destReposDict[getDestNameFromSourceName(sourceRepo.Name)];
+
+                // Create a temp dir to store the cloned repo
+                var gitDir = Directory.CreateDirectory(Path.Join(tempDir.FullName, sourceRepo.Name));
+
+                var co = new CloneOptions();
+                CredentialsHandler sourceCredProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = pat1, Password = pat1 };
+                co.CredentialsProvider = sourceCredProvider;
+
+
+                Console.WriteLine($"Cloning source {sourceRepo.Name}");
+                Repository.Clone(sourceRepo.RemoteUrl, gitDir.FullName, co);
+
+                using (var repo = new Repository(gitDir.FullName))
+                {
+                    CredentialsHandler destCredProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = pat2, Password = pat2 };
+                    LibGit2Sharp.PushOptions options = new LibGit2Sharp.PushOptions();
+                    options.CredentialsProvider = destCredProvider;
+
+                    if (!repo.Network.Remotes.Where(x => x.Name == "dest").Any())
+                    {
+                        repo.Network.Remotes.Add("dest", destRepo.RemoteUrl);
                     }
                     else
                     {
-                        pr.Reviewers = new List<IdentityRefWithVote>() { new IdentityRefWithVote(new IdentityRef { Id = reviewerToAssign.UserId }) };
+                        repo.Network.Remotes.Remove("dest");
+                        repo.Network.Remotes.Add("dest", destRepo.RemoteUrl);
+                    }
+
+                    var destRemote = repo.Network.Remotes["dest"];
+
+                    foreach (var branch in repo.Branches)
+                    {
+                        Console.WriteLine($"Pusing source {sourceRepo.Name}:{branch} to {destRepo.Name}:{branch} @ {destRepo.Url}");
+
+                        repo.Network.Push(destRemote, branch.CanonicalName, new PushOptions()
+                        {
+                            CredentialsProvider = destCredProvider
+                        });
                     }
 
                 }
-
             }
+
+
         }
 
-        static private ReviewersHistory GetNextReviewer(IEnumerable<ReviewersHistory> reviewersHistory)
+        private static string getDestNameFromSourceName(String x)
         {
-            var availableUsers = reviewersHistory.Where(user => user.ActiveReviewsCount < 2);
-            var sortedByHistory = availableUsers.OrderBy(user => user.TotalReviews);
-
-            var reviewerToAssign = sortedByHistory.FirstOrDefault();
-            if (reviewerToAssign == null)
-            {
-                return null;
-            }
-            reviewerToAssign.ActiveReviewsCount++;
-
-            return reviewerToAssign;
+            return "export_" + x;
         }
 
-        private static Dictionary<string, ReviewersHistory> BuildHistory(IEnumerable<PullRequest> prs)
-        {
-            var reviewersHistory = new Dictionary<string, ReviewersHistory>();
-
-            // Build history 
-            foreach (var pr in prs)
-            {
-                foreach (var reviwer in pr.Reviewers)
-                {
-                    // Build `pastReviewCount`
-                    if (pr.PR.Status == PullRequestStatus.Completed)
-                    {
-                        if (reviewersHistory.ContainsKey(reviwer.Id))
-                        {
-                            reviewersHistory[reviwer.Id].PastReviewCount++;
-                        }
-                        else
-                        {
-                            reviewersHistory[reviwer.Id] = new ReviewersHistory
-                            {
-                                UserId = reviwer.Id,
-                                PastReviewCount = 1,
-                            };
-                        }
-                    }
-
-                    // Build `activeReviewCount`
-                    if (pr.PR.Status == PullRequestStatus.Active)
-                    {
-                        if (reviewersHistory.ContainsKey(reviwer.Id))
-                        {
-                            reviewersHistory[reviwer.Id].ActiveReviewsCount++;
-                        }
-                        else
-                        {
-                            reviewersHistory[reviwer.Id] = new ReviewersHistory
-                            {
-                                UserId = reviwer.Id,
-                                ActiveReviewsCount = 1,
-                            };
-                        }
-                    }
-                }
-            }
-
-            return reviewersHistory;
-        }
-
-        static private async Task<IEnumerable<PullRequest>> GetAllActivePRs(VssConnection connection, String project)
+        static private async Task<IEnumerable<GitRepository>> GetAllRepos(VssConnection connection, String project)
         {
             var sourceClient = connection.GetClient<GitHttpClient>();
             var repos = await sourceClient.GetRepositoriesAsync(project);
+            return repos;
+        }
 
-            // var allPRs = new List<GitPullRequest>();
-            // var querySettings = new GitPullRequestSearchCriteria();
-            // // querySettings.Status = PullRequestStatus.All;
-
-            var allPRs = new List<PullRequest>();
-
-            foreach (var repo in repos)
-            {
-
-                Console.WriteLine($"Getting all Active PRs for '{repo.Name}'");
-
-                var prs = await sourceClient.GetPullRequestsAsync(project, repo.Id.ToString(), new GitPullRequestSearchCriteria() { Status = PullRequestStatus.All, IncludeLinks = true });
-                foreach (var pr in prs)
-                {
-                    Console.WriteLine($"Getting reviewers for '{pr.Title}'");
-
-                    var reviews = await sourceClient.GetPullRequestReviewersAsync(repo.Id.ToString(), pr.PullRequestId);
-                    allPRs.Add(new PullRequest
-                    {
-                        PR = pr,
-                        Reviewers = reviews
-                    });
-                }
-
-            }
-
-            Console.WriteLine("Got PRs");
-            return allPRs;
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
+
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Very basic hack looking at round robin style assignment of users for PRs.
+Very basic hack looking at moving repositories from one Azure devops to another in an automated fashion.
+
+Running: 
+
+1. Clone
+2. Open in VSCode devcontainer
+3. Copy `example.env` -> `.env` and fill out details of source (proj1/org1/pat1) and dest (proj2/org2/pat2)
+4. Press F5 to start
+
+This is untested - don't use without reviewing the code and giving least privilidge
